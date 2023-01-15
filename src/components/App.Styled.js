047ea35914ba845c403523e25c540d4a39736414
MythosOne <107143884+MythosOne@users.noreply.github.com>
2023-01-15 20:47:10 +0200

    goit-react-hw-02-feedback-styled

---
+++ src/components/App.Styled.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export  const Fragment = styled.div`
+width: 240px;
+margin: 0 auto;
+border: 1px solid;
+border-radius: 4px;
+box-shadow: 0px 1px 3px rgb(0, 0, 0, 0.12);
+`;
+
+export const Section = styled.h2`
+  
+`;
+
+export const FeedbackOptions = styled.button`
+  
+`;
+
+export const Notification = styled.p`
+  
+`;
+
+export const Statistics = styled.ul`
+  
+`;
+
+
+
+// element.style {
+//     width: 240px;
+//     margin: 0 auto;
+//     border: 1px solid;
+//     border-radius: 4px;
+// }
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,57 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component, Fragment } from 'react';
+import Statistics from './Statistics';
+import FeedbackOptions from './FeedbackOptions';
+import Section from './Section';
+import Notification from './Notification';
+// import { Fragment, Section, FeedbackOptions, Notification, Statistics } from './App.Styled';
+export class App extends Component {
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+    };
+
+    onleaveFeedback = event => {
+        const name = event.target.name;
+        this.setState(prevState => ({
+            [name]: prevState[name] + 1
+        }));
+    }
+
+    countTotalFeedback = () => {
+        const { good, neutral, bad } = this.state;
+        return good + neutral + bad;
+    }
+
+    countPositiveFeedbackPercentage = () => {
+        const { good } = this.state;
+        const total = this.countTotalFeedback();
+        return total === 0 ? '0' : Math.round((good / total) * 100);
+    }
+
+    render() {
+        const { good, neutral, bad } = this.state;
+        const total = this.countTotalFeedback();
+        const positivePerc = this.countPositiveFeedbackPercentage();
+        const objKey = Object.keys(this.state);
+        
+        return (
+          <Fragment>
+                <Section title="Please leave feedback">
+                <FeedbackOptions options={objKey} onLeaveFeedback={this.onleaveFeedback}/>
+                </Section>
+                {total === 0 ? 
+                    (<Notification message="There is no feedback" />) :
+                (<Section title="Statistics">
+                <Statistics
+                    good={good}
+                    neutral={neutral}
+                    bad={bad}
+                    total={total} positivePercentage={positivePerc}/>
+                </Section>)}
+            </Fragment>
+        );
+    };
 };
+
+export default App
